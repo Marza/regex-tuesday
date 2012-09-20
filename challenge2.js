0946f152af5ab9f3be582f5792d714b86f8dd654
Tony Marjakangas <tony@marza.se>
2012-09-20 08:13:36 +0200

    separate regex for rgb/rgba colors

--- challenge2.js
+++ challenge2.js
@@ -1,2 +1,6 @@
 /* WIP! http://callumacrae.github.com/regex-tuesday/challenge2.html */
+/* hex colors */
 /^#([a-f0-9]{1,2})\1\1$/i
+
+/* rgb/rgba colors */
+/^rgb\(0*([0-9]{0,2}(\.[0-9])?%?),\s*0*\1,\s*0*\1\)|rgba\(0*([0-9]{0,2}(\.[0-9])?%?),\s*0*\3,\s*0*\3,0*([0-9]{0,2}(\.[0-9]+)?%?)\s*\)$/i
